c0c149426113a6cd1ce9044b64c032375bcaf457
Harjap Singh <157319482+Harjap-Sngh@users.noreply.github.com>
2025-05-09 23:53:34 -0600

    reversing the previous change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import ResumeForm from "@/components/ResumeForm";
 export default function Home() {
   return (
     <main className="min-h-screen bg-gray-100 py-10">
-      <div>THis is for making the rsume by autmation</div>
       <ResumeForm />
     </main>
   );
